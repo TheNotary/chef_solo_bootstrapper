2221dc2d35f894a36355a398214f4890a75c0bc7
TheNotary <no@email.plz>
2016-08-16 21:49:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chef Solo Bootstrapper Template
+
+Make a copy of this repo for each server you want.  Then configure that copy to your liking.  Then apply the bootstrapping to the new VM you create.  
+
+To apply the bootstrapping, follow these steps:
+
+1.  Create a new VM with some username
+2.  Allow passwordless sudo for that username `myusername ALL=(ALL) NOPASSWD: ALL`
+3.  Setup a .ssh/config entry for logging into the new machine
+4.  Run `./deploy.sh new-server-host`
+
+
+Ref:  http://www.opinionatedprogrammer.com/2011/06/chef-solo-tutorial-managing-a-single-server-with-chef/
---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This runs as root on the server
+
+# chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
+# chef_binary=/usr/local/bin/chef-solo
+chef_binary=/usr/bin/chef-solo
+
+# Are we on a vanilla system?
+if ! test -f "$chef_binary"; then
+    #export DEBIAN_FRONTEND=noninteractive
+    # Upgrade headlessly (this is only safe-ish on vanilla systems)
+    #aptitude update &&
+    apt-get -o Dpkg::Options::="--force-confnew" \
+        --force-yes -fuy dist-upgrade &&
+    # Install Ruby and Chef
+    #aptitude install -y ruby1.9 ruby-dev gcc make &&
+    #sudo gem install --no-rdoc --no-ri chef --version 0.12.6
+
+    aptitude install -y gcc make curl &&
+    sudo su -c 'curl -L https://www.opscode.com/chef/install.sh | bash'
+fi &&
+
+"$chef_binary" -c solo.rb -j solo.json
